ed8e7521f02cb926dd4c77c60378acb55da3c0e8
Toto ZEHERO <sylvain.peysson@gmail.com>
2018-11-28 11:38:03 +0100

    rajout TEST ligne 25 du Main

--- app/src/main/java/com/hbv1/myclub/MainActivity.kt
+++ app/src/main/java/com/hbv1/myclub/MainActivity.kt
@@ -21,6 +21,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+
+//            test Sylvain
         }
 
         val toggle = ActionBarDrawerToggle(
